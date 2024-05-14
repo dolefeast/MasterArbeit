899530dddeb5bccb92e8682a19f3204f4ed41341
Dolefeast <elrois456yt@gmail.com>
2024-05-14 17:30:09 +0200

    removed calculations

--- calculations/math_objects/get_eigenvalues.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-
-def get_eigenvalues(eigenstates):
-    return np.array([eigenstate.p[0] for eigenstate in eigenstates])
--- calculations/test_iterable.py
+++
@@ -1,9 +0,0 @@
-from math_objects.iterable_output import iterable_output
-
-
-@iterable_output
-def identity(x):
-    return x
-
-
-print(list(map(identity, range(5))))
