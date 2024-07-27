321d1c40da1b9e6068a3470a1961a310af524a2b
Dolefeast <elrois456yt@gmail.com>
2024-07-27 12:54:31 +0200

    added some saving and reading routines

--- computation/minimal_working_example.py
+++ computation/minimal_working_example.py
@@ -1,10 +1,14 @@
 import matplotlib.pyplot as plt
 from mpmath import quad
 import numpy as np
+from numpy import savetxt
 from scipy.integrate import solve_bvp
 from scipy.interpolate import CubicSpline
 
 from itertools import cycle
+
+#TODO: 
+# - Organize script into a single function
 
 def sign(x):
     # Returns sign of x
@@ -17,7 +21,6 @@ def float_in_array(x, array, tol=1e-3):
             return True
     return False
 
-
 def A0(z):
     # This is horrible I know. Usually all this is done inside a class
     # A0_induced should be a CubicSpline
@@ -29,6 +32,8 @@ def eigenvalue_guess(n):
 
 def dirichlet_eigenstate(z, omega_n):
     # A first order approximation of a solution guess for the Klein-Gordon equation
+    if omega_n==0:
+        return np.zeros_like(z)
     n = sign(omega_n) / np.pi
 
     if n == 0:
@@ -45,6 +50,8 @@ def dirichlet_eigenstate(z, omega_n):
     return eigenstate
 
 def dirichlet_eigenstate_gradient(z, omega_n):
+    if omega_n==0:
+        return np.zeros_like(z)
     n = sign(omega_n) / np.pi
 
     if n == 0:
@@ -92,37 +99,54 @@ def Dirichlet_bcs(ya, yb, p):
     bcs = np.array((ya[0], yb[0], yb[1] - 1))
     return bcs
 
-def calculate_eigenstates(max_N):
+def calculate_eigenstates(max_N,
+        max_nodes=1e20,
+        tol=1e-2,
+        ):
 
     eigenvalues = []
     eigenstates = []
     eigenstate_gradients = []
 
-    for n in range(-max_N, max_N+1):
-        if n == 0:
+    eigenvalue_guess_array = solution_family["eigenvalues"]
+    eigenstate_guess_array = solution_family["eigenstates"]
+    eigenstate_gradient_guess_array = solution_family["eigenstate_gradients"]
+
+    for (
+            eigenvalue_guess,
+            eigenstate_guess,
+            eigenstate_gradient_guess
+            ) in zip(
+                    eigenvalue_guess_array,
+                    eigenstate_guess_array,
+                    eigenstate_gradient_guess_array,
+                    ):
+        if eigenvalue_guess == 0:
             continue # Non physical case
 
-        omega_n = eigenvalue_guess(n) # the eigenvalue guess
+        # omega_n = eigenvalue_guess[n] # the eigenvalue guess
 
         # The guess for the solution, and its derivative, as
         # its solving a 2nd order differential eq
         y = (
-                dirichlet_eigenstate(z, omega_n),
-                dirichlet_eigenstate_gradient(z, omega_n),
+                eigenstate_guess,
+                eigenstate_gradient_guess,
                 )
         eigenstate = solve_bvp(
                 Klein_Gordon_equation,
                 Dirichlet_bcs,
                 z, # The x-axis,
                 y,
-                p=(omega_n,),
-                max_nodes=9e19,
+                p=(eigenvalue_guess,),
+                max_nodes=max_nodes,
+                tol=tol,
                 )
         omega_n = eigenstate.p[0]
 
         if not eigenstate.success:
             # Sometimes the ODE doesnt converge
-            raise Exception(f"Solution associated with omega_n={omega_n} did not converge")
+            #raise Exception(f"Solution associated with omega_n={omega_n} did not converge")
+            raise Exception(eigenstate.message)
 
         if float_in_array(omega_n, eigenvalues):
             # Sometimes an eigenvalue converges to another possible solution
@@ -283,10 +307,102 @@ def calculate_A0_total(A0_induced):
     # A0_induced is a function
     return lambda z: -lambda_value * (z - 1/2) + a*A0_induced(z)
 
+def generate_solution_family_guess(lambda_value, max_N):
+    lambda_value = lambda_value # From the way the guess functions work
+    eigenvalue_guess_array = [
+            eigenvalue_guess(n) for n in range(-max_N, max_N+1)
+            ]
+    eigenstate_guess_array = [
+            dirichlet_eigenstate(z, omega_n) 
+            for omega_n in eigenvalue_guess_array
+            ]
+    eigenstate_gradient_guess_array = [
+            dirichlet_eigenstate_gradient(z, omega_n) 
+            for omega_n in eigenvalue_guess_array
+            ]
+
+    solution_family = {
+            "eigenvalues":eigenvalue_guess_array,
+            "eigenstates":eigenstate_guess_array,
+            "eigenstate_gradients":eigenstate_gradient_guess_array,
+            }
+
+    return solution_family
+
+def float_to_str(value, sig_digs=3):
+    return str(
+        round(
+            float(
+                value
+            ),
+            sig_digs
+        )
+    ).replace(".", "_")
+
+def save_solutions(
+        converged_dictionary,
+        directory="",
+        sig_digs=3,
+        ):
+    """
+    Saves the calculated quantities to {directory}/{boundary_conditions}/{quantity}/a_{a}_mass_{mass}_lambda_value_{lambda_value}.csv
+    Parameters:
+        solution_family: A dictionary with keys() = ["eigenvalues", "eigenstates", "eigenstate_gradients"]
+        directory: In case a further directory should be considered, e.g. if Ambjorn technique is used
+    Returns None
+    """
+    lambda_string = float_to_str(lambda_value, sig_digs=sig_digs)
+    a_string = float_to_str(a, sig_digs=sig_digs)
+    m_string = float_to_str(m, sig_digs=sig_digs)
+
+    file_id = f"mass_{m_string}_a_{a_string}_lambda_{lambda_string}.txt"
+
+    if directory != "":
+        directory = "/" + directory
+
+    root_directory = f"saved_solutions{directory}/{bcs}"
+    print(f"Saving results under {root_directory}/{converged_dictionary.keys()}/{file_id}...\n")
+    
+    for key, value in converged_dictionary.items():
+        savetxt(
+                f"{root_directory}/{key}/{file_id}",
+            value,
+            delimiter=",",
+        )
+
+def read_solutions_from_file(
+        directory="",
+        sig_digs=3,
+        ):
+    m_string = float_to_str(m, sig_digs)
+    lambda_string = float_to_str(lambda_value, sig_digs)
+    a_string = float_to_str(a, sig_digs)
+
+    file_id = f"mass_{m_string}_a_{a_string}_lambda_{lambda_string}.txt"
+
+    if directory != "": 
+        directory = "/" + directory
+
+    # Initialize the solution family dict
+    solution_family = {
+            "eigenvalues":0,
+            "eigenstates":0,
+            "eigenstate_gradients":0,
+            "rho":0,
+            "A0_induced":0,
+            }
+    
+    for key in solution_family.keys():
+        solution_family[key]  = np.genfromtxt(
+            f"saved_solutions{directory}/{bcs}/{key}/{file_id}",
+            dtype=float,
+            delimiter="\n",
+        )
+
+    return solution_family
 
 if __name__ == "__main__":
-
-    plot = True
+    plot = False
     if plot:
         fig, (ax_rho, ax_A0_induced, ax_eigenvalues) = plt.subplots(3)
         eigenvalues_array = []
@@ -298,11 +414,20 @@ if __name__ == "__main__":
     z = np.linspace(0, 1, n_points)
 
     a = 1
+    m = 0
     max_N = 1 #24 # Cutoff for the mode solutions
                # Note that there will be 2 * max_N - 1 solutions
     lambda_min = 7 # Strength of the external electric field
     lambda_max = 25 # Strength of the external electric field
-    lambda_step=0.25
+    lambda_step=0.1
+
+    tol = 1e-3
+    max_nodes = 5e15
+    smoothing = False
+    directory = "ambjorn"
+    bcs = "dirichlet"
+    save = True
+    read = False
 
     n_iterations = 15 # Number of iterations for each value of lambda
 
@@ -310,20 +435,40 @@ if __name__ == "__main__":
     A0_induced = CubicSpline(z, np.zeros_like(z))
     color_cycle = cycle(["#FE5F55","#3423A6","#710627","#7180B9","#163B20"])
 
+    lambda_value = lambda_min
+
+    if read:
+        try: 
+            raise Exception("Solutions can't be read yet, eigenstates aren't read correctly")
+            solution_family = read_solutions_from_file(
+                    directory=directory,
+                    )
+            A0_induced = solution_family["A0_induced"]
+        except FileNotFoundError:
+            solution_family = generate_solution_family_guess(lambda_min, max_N)
+            A0_induced = CubicSpline(z, np.zeros_like(z))
+            pass
+    else:
+        solution_family = generate_solution_family_guess(lambda_min, max_N)
+        A0_induced = CubicSpline(z, np.zeros_like(z))
+
     for i, lambda_value in enumerate(np.arange(lambda_min, lambda_max, lambda_step)):
-        print('λ =', lambda_value)
+        print('λ= ', lambda_value)
         color = next(color_cycle) # To distinguish between different lambda values
         if lambda_value == 17.0:
             print('We got here, increasing n_iterations')
             n_iterations *= 2
         try:
             for n in range(n_iterations):
-                solution_family = calculate_eigenstates(max_N)
+                solution_family = calculate_eigenstates(max_N,
+                        max_nodes=max_nodes,
+                        tol=tol,)
 
                 normalized_solution_family = normalize_eigenstate_family(solution_family)
 
                 rho = calculate_rho(normalized_solution_family)
-                # rho = filter_rho(rho)
+                if smoothing:
+                    rho = filter_rho(rho)
                 A0_induced = calculate_A0_induced(rho)
 
                 A0 = calculate_A0_total(A0_induced)
@@ -335,8 +480,18 @@ if __name__ == "__main__":
             if plot:
                 eigenvalues_array.append(solution_family["eigenvalues"])
                 lambda_array.append(lambda_value)
-                plot_rho_A0_induced(rho, A0_induced(z), '', color=color, alpha=1)
-            # print(f'n={n_iterations}, max(A0_induced) =', max(A0_induced(z)))
+                if not i%3:
+                    plot_rho_A0_induced(rho, A0_induced(z), '', color=color, alpha=1)
+
+            if save:
+                solution_family["rho"] = rho
+                solution_family["A0_induced"] = A0_induced(z)
+                save_solutions(
+                    solution_family,
+                    directory=directory,
+                    )
+
+            print(f'n={n_iterations}, max(A0_induced) =', max(A0_induced(z)))
             A0 = calculate_A0_total(lambda z: (lambda_value + lambda_step)/lambda_value * A0_induced(z))
 
         except Exception as exception:
@@ -351,4 +506,3 @@ if __name__ == "__main__":
         plt.show()
     
     raise e
-
